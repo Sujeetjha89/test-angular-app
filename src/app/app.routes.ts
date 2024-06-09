8fe2fa5630993a5f8e89be99da64561c845e0c44
Sujeetjha89 <sujeet.j@instantpay.in>
2024-06-09 13:15:39 +0530

    wip:before updating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/index' },
+  { path: 'index', component:HomeComponent },
+  { path: 'about', component:WelcomeComponent },
+  { path: 'services', component:ServicesComponent },
+  { path: 'contact', component:ContactComponent }
+];
+
+
